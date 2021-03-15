bdb4f003d5bdd89c060d23956cc12003bb5980ed
Lorenzo Maturano <maturanolorenzo@gmail.com>
2021-03-15 16:26:03 -0300

    getting matrix dimension

--- main.hs
+++ main.hs
@@ -1,14 +1,21 @@
 import System.IO
 import Control.Monad
 
+-- Get puzzle dimension
+dimension :: [t]-> t
+dimension (n:_) = n
+
 main = do
-  let list = []
+  -- Aloca e retornar um novo manipulador para gerenciar o arquivo
   handle <- openFile "input.txt" ReadMode
   contents <- hGetContents handle
-  let singleWords = words contents
-      list = f singleWords
-  print list
-  hClose handle
+  let arr = words contents
 
-f :: [String] -> [Int]
-f = map read
+  -- Get Puzzle dimension
+  let n_str = dimension arr
+  print n_str
+  let n = (read n_str :: Int)
+  print n
+
+  -- Fecha o handle
+  hClose handle
