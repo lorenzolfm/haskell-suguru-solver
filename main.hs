9077d90148aab5002e386b746a619dc50e841945
Lorenzo Maturano <maturanolorenzo@gmail.com>
2021-03-15 15:19:19 -0300

    reading input.txt

--- main.hs
+++ main.hs
@@ -1,2 +1,14 @@
-main :: IO ()
-main = putStrLn "Hello World"
+import System.IO
+import Control.Monad
+
+main = do
+  let list = []
+  handle <- openFile "input.txt" ReadMode
+  contents <- hGetContents handle
+  let singleWords = words contents
+      list = f singleWords
+  print list
+  hClose handle
+
+f :: [String] -> [Int]
+f = map read
