a98aaa6fc76484e2fee65c74dee32f0abf2bfdb7
Lorenzo Maturano <maturanolorenzo@gmail.com>
2021-03-20 15:28:31 -0300

    apagando arquivos, começando de novo

--- Types.hs
+++
@@ -1,8 +0,0 @@
-module Types (Position) where
-
-type Position = (Int, Int)
-type PossibleValues = [Int]
-type GroupId = Int
-type Group = (GroupId,[(Int, Int)])
-
-type Cell = (Position, PossibleValues, Group)
